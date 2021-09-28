e222b54b22b84c22c76d7be3d295e4c66420c6cf
Juan Ríos <juandarr@gmail.com>
2021-09-28 18:54:05 -0500

    add: new codemirror component adapted. Included vim mode and basic definition of test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,41 @@
+/* eslint-disable react/react-in-jsx-scope */
 import "./App.css";
+import 'codemirror/keymap/vim';
+import React from 'react';
+import CodeMirror from '@uiw/react-codemirror';
+import 'codemirror/theme/dracula.css';
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        h1 > Simple Vim tutorial < /h1> <
-        p >
-        Edit < code > src / App.js < /code> and save to reload.{" "} <
-        /p>{" "} <
-        /header>{" "} <
-        /div>
+    const objectPositioned = ' '.repeat(Math.floor(Math.random(10) * 10)) + '%' + ' '.repeat(Math.floor(Math.random(10) * 10)) + '\n';
+    const upperLines = ' '.repeat(20) + '\n'.repeat(Math.floor(Math.random(10) * 10));
+    const lowerLines = ' '.repeat(20) + '\n'.repeat(Math.floor(Math.random(10) * 10));
+    const vimTest1 = objectPositioned + upperLines + lowerLines;
+    return (<div className="App">
+        <header className="App-header">
+            <h1 > Simple Vim tutorial - Delete</h1>
+            <p className="pb-8">
+                Edit <code> src / App.js </code> and save to reload.
+            </p>
+            <div className="left-20 right-20 top-60 bottom-20 text-left">
+                <CodeMirror
+                    options={{
+                        theme: 'dracula',
+                        mode: 'js',
+                        keyMap: 'vim'
+                    }}
+                    value={vimTest1}
+                    height="300px"
+                    onChange={(editor) => {
+                        if (editor.getValue().includes('%')) {
+                            //
+                        } else {
+                            console.log('value:', 'You win!');
+                        }
+
+                    }}
+                /> </div>
+        </header>
+    </div>
     );
 }
 
